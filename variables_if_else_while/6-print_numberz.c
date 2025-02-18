a15a1a46af598a6eb36b5d9cb6a87820a18f69cd
cmeynet <clemence.meynet14@gmail.com>
2025-02-18 13:46:14 +0000

    Print all single digit numbers of base 10 with putchar function without type char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Loop prints all single digit numbers of base 10 without char
+ *
+ * Return: the digit numbers of base 10
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
