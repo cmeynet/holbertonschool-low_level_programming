995e3a443be2ab03506420fbe32175c89ad8cc47
cmeynet <clemence.meynet14@gmail.com>
2025-02-18 11:25:50 +0000

    Print alphabet with putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main - Loop for print the alphabet in lowercase
  *
  * Return: the alphabet in lowercase
@@ -7,11 +7,11 @@
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
