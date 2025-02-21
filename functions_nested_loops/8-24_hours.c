97afd61ccc40d0a71c31b291720e1b346ea0b1dc
cmeynet <clemence.meynet14@gmail.com>
2025-02-21 09:48:04 +0000

    Function that prints the last digit of a number

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
  * print_last_digit - Function to print the last digit of a number
  * @n: number to checked
  * Return: last digit of a number
  */
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-	_putchar('0' + abs(n % 10));
-	return (abs(n % 10));
 }
