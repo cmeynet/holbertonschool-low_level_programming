91938dfcfbee8342124e0dff768d2d78418b80e2
cmeynet <clemence.meynet14@gmail.com>
2025-02-21 12:54:47 +0000

    Function that prints every minute of the day of Jack Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_last_digit - Function to print the last digit of a number
- * @n: number to checked
- * Return: last digit of a number
+ * jack_bauer - Function to print every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * Return: every minute of the day of Jack Bauer
  */
 void jack_bauer(void)
 {
+	int hours, minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(':');
+			_putchar('0' + minutes / 10);
+			_putchar('0' + minutes % 10);
+			_putchar('\n');
+		}
+	}
 }
