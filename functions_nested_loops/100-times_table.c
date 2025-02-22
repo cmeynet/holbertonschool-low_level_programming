af50024b009416e690ff14ab42d17e8a86941f31
cmeynet <clemence.meynet14@gmail.com>
2025-02-22 22:43:00 +0000

    Test_v2

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,23 +7,16 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-	
-	if (n > 0 && n < 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = i * j;
-				if (k < n)
-				{
-				_putchar('0' + k);
-				_putchar(' ');
-				}
+	int i, j, product;
 
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j <= 9; j++)
+			{
+			product = n * i * j;
+				_putchar('0' + product);
+				_putchar(' ');
 			}
 		}
-	}
 		_putchar('\n');
 }
