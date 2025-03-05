712ad3cecf55797190c19c7efbacfa4ee866c154
cmeynet <clemence.meynet14@gmail.com>
2025-03-05 20:07:24 +0100

    Program that print its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Print name of a program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if it's correct
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
