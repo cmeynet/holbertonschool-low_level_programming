9d6addd10bbc2e67f5c35d6c1fbeb8750718c5c1
cmeynet <clemence.meynet14@gmail.com>
2025-03-12 13:04:28 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Combine data items of
+ * different kinds
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: ID of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
