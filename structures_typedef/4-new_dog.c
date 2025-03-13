bd726a7be3353537dc0b964a4b61bfd0ed8967f1
cmeynet <clemence.meynet14@gmail.com>
2025-03-13 20:19:38 +0100

    new_dog revision

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,43 +10,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-	char *ptr_name, *ptr_owner;
 	int i, len_name = 0, len_owner = 0;
 
+	/* Allocate memory for the structure */
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
+
 	while (name[len_name] != '\0')
 		len_name++;
-	if (name != NULL)
+
+	ptr->name = malloc(sizeof(char) * (len_name + 1));
+	if (ptr->name == NULL)
 	{
-		ptr_name = malloc(sizeof(char) * (len_name + 1));
-		if (ptr_name == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		for (i = 0; i < len_name; i++)
-			ptr_name[i] = name[i];
-		ptr_name[len_name] = '\0';
-	ptr->name = ptr_name;
+		free(ptr);
+		return (NULL);
 	}
+	/* To copy name in ptr->name */
+	for (i = 0; i < len_name; i++)
+		ptr->name[i] = name[i];
+	ptr->name[len_name] = '\0';
+
 	while (owner[len_owner] != '\0')
 		len_owner++;
-	if (owner != NULL)
+
+	ptr->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (ptr->owner == NULL)
 	{
-		ptr_owner = malloc(sizeof(char) * (len_owner + 1));
-		if (ptr_owner == NULL)
-		{
-			free(ptr_name);
-			free(ptr);
-			return (NULL);
-		}
-		for (i = 0; i < len_owner; i++)
-			ptr_owner[i] = owner[i];
-		ptr_owner[len_owner] = '\0';
-	ptr->owner = ptr_owner;
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
 	}
+
+	for (i = 0; i < len_owner; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner[len_owner] = '\0';
+
 	ptr->age = age;
 	return (ptr);
 }
