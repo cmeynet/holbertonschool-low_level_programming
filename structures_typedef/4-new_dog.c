caef776da0010846fe486eb8549c8da22362e2f3
cmeynet <clemence.meynet14@gmail.com>
2025-03-13 15:15:55 +0100

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+/**
+ * new_dog - Function that create a new dog
+ * @name: name of the dog
+ * @age: age the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to a new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+	char *ptr_name, *ptr_owner;
+	int i, len_name = 0, len_owner = 0;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	while (name[len_name] != '\0')
+		len_name++;
+
+	if (name != NULL)
+	{
+		ptr_name = malloc(sizeof(char) * (len_name + 1));
+
+		if (ptr_name == NULL)
+			return (NULL);
+		for (i = 0; i < len_name; i++)
+			ptr_name[i] = name[i];
+		ptr_name[len_name] = '\0';
+	}
+	ptr->name = ptr_name;
+
+	while (owner[len_owner] != '\0')
+		len_owner++;
+
+	if (owner != NULL)
+	{
+		ptr_owner = malloc(sizeof(char) * (len_owner + 1));
+		if (ptr_owner == NULL)
+			return (NULL);
+		for (i = 0; i < len_owner; i++)
+			ptr_owner[i] = owner[i];
+		ptr_owner[len_owner] = '\0';
+	}
+	ptr->owner = ptr_owner;
+	ptr->age = age;
+	return (ptr);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - Combine data items of
@@ -23,5 +24,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
