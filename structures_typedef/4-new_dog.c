c30b33f7c041b6f2c43e20d3f95a9e19723034e4
cmeynet <clemence.meynet14@gmail.com>
2025-03-13 15:47:18 +0100

    Correction new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,36 +16,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-
 	while (name[len_name] != '\0')
 		len_name++;
-
 	if (name != NULL)
 	{
 		ptr_name = malloc(sizeof(char) * (len_name + 1));
-
 		if (ptr_name == NULL)
+		{
+			free(ptr);
 			return (NULL);
+		}
 		for (i = 0; i < len_name; i++)
 			ptr_name[i] = name[i];
 		ptr_name[len_name] = '\0';
 	ptr->name = ptr_name;
 	}
-
 	while (owner[len_owner] != '\0')
 		len_owner++;
-
 	if (owner != NULL)
 	{
 		ptr_owner = malloc(sizeof(char) * (len_owner + 1));
 		if (ptr_owner == NULL)
+		{
+			free(ptr_name);
+			free(ptr);
 			return (NULL);
+		}
 		for (i = 0; i < len_owner; i++)
 			ptr_owner[i] = owner[i];
 		ptr_owner[len_owner] = '\0';
 	ptr->owner = ptr_owner;
 	}
-
 	ptr->age = age;
 	return (ptr);
 }
