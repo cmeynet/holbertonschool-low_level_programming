8025007817de79fa0dbeea75a524b03d8cbf972e
cmeynet <clemence.meynet14@gmail.com>
2025-03-21 11:54:14 +0100

    Update hello_wolrd.asm : add bits in 64

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,19 @@
+bits 64
+global main ; Make the 'main' function accessible by the linker
+
 section .data
-message db "Hello, world!", 10, 0  ; String followed by a new line
+Hello db "Hello, World", 10 ; String followed by a new line
 
 section .text
-    global main ; Make the 'main' function accessible by the linker
-    extern printf  ; Declare printf (provided by libc)
-
 main:
-    sub rsp, 8     ; The stack must be 16-byte aligned (RSP must be a multiple of 16)
-    mov rdi, message ; Put the message address in RDI
-    call printf
-    add rsp, 8     ; Restore RSP
-    ret  ; Return to the caller (here, libc, which manages the execution of the program)
+    mov rax, 1 ; sys_write
+    mov rdi, 1 ; stdout
+    mov rsi, Hello
+    mov rdx, 13
+    syscall
+
+    mov rax, 60 ; sys_exit
+    xor rdi, rdi ; exit(0)
+    syscall
 
 section .note.GNU-stack noalloc noexec nowrite progbits ; Disable the executable stack for security
